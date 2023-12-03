c06d4e399d301640ff9aba214cf594d103ae3613
Andrea Baragiola <andrea.baragiola@outlook.it>
2023-12-03 17:30:51 +0100

    edits

--- SimpleClient/Program.cs
+++ SimpleClient/Program.cs
@@ -2,21 +2,30 @@
 using System;
 using SimpleTCPChatLibrary;
 
-Console.Write("Inserisci l'indirizzo IP del server: ");
-string serverIp = Console.ReadLine();
-
-SimpleChatClient chatClient = new SimpleChatClient();
-chatClient.MessageReceived += (message) => Console.WriteLine(message);
-
-// Connessione al server
-chatClient.Connect(serverIp, 8888);
-
-// Loop per inviare messaggi al server
-while (true)
+class Program
 {
-    Console.Write("Messaggio: ");
-    string message = Console.ReadLine();
+    static void Main()
+    {
+        Console.Write("Inserisci l'indirizzo IP del server: ");
+        string serverIp = Console.ReadLine();
 
-    // Invia il messaggio al server
-    chatClient.SendMessage(message);
+        SimpleChatClient chatClient = new SimpleChatClient();
+        //chatClient.MessageReceived += (message) => Console.WriteLine(message);
+
+        // Connessione al server
+        chatClient.Connect(serverIp, 8888);
+
+        // Avvia il thread per l'ascolto continuo dei messaggi dal server
+        Task.Run(() => chatClient.StartListening());
+
+        // Loop per inviare messaggi al server
+        while (true)
+        {
+            Console.Write("Messaggio: ");
+            string message = Console.ReadLine();
+
+            // Invia il messaggio al server
+            chatClient.SendMessage(message);
+        }
+    }
 }
